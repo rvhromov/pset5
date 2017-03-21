dd381a9a868d485bc5f110e459c1aa1a96d2cf63
rvhromov <rv.hromov@gmail.com>
2017-03-21 10:11:08 +0200

    Spell-checks a file

--- Speller/speller.cpp
+++ Speller/speller.cpp
@@ -4,24 +4,21 @@
 using namespace std;
 
 char *getFile();
-bool openForReading(FILE *f);
+bool openForReading(FILE *fp);
+void spellCheck(FILE *fp, Dictionary *dicts);
 void igonoreBigWords(FILE *fp, char c, int index);
-bool checkWord(int index, char *word, Dictionary *d);
+bool checkWord(int index, char *word, Dictionary *dicts);
+void showResults(int misspellings, int words, Dictionary *dicts);
 
 int main()
 {
-    int index = 0, misspellings = 0, words = 0;
-    char word[LENGTH + 1];
-
     Dictionary *dicts = new Dictionary();
-
     // Загрузить словарь в память
     if(!dicts -> load("large"))
     {
         cout << "Could not load dictionary" << endl;
         return 1;
     }
-
      // Открыть файл с текстом
     FILE *fp = fopen(getFile(), "r");
     if (openForReading(fp))
@@ -32,6 +29,37 @@ int main()
 
     cout << "\nMISSPELLED WORDS:\n\n";
 
+    spellCheck(fp, dicts);
+    fclose(fp);
+    dicts -> unload();
+
+    delete dicts;
+    return 0;
+}
+
+char *getFile()
+{
+    char *text = new char(LENGTH);
+    cout << "Enter the name of a file you'd like to check: ";
+    cin >> text;
+    return text;
+}
+
+bool openForReading(FILE *fp)
+{
+    if (fp == NULL)
+    {
+        cout << "Could not open a text. Usage: file.txt" << endl;
+        return true;
+    }
+
+    return false;
+}
+
+void spellCheck(FILE *fp, Dictionary *dicts)
+{
+    int index = 0, misspellings = 0, words = 0;
+    char word[LENGTH + 1];
     // Проверить правописание кажого слова в тексте
     for (int c = fgetc(fp); c != EOF; c = fgetc(fp))
     {
@@ -41,7 +69,6 @@ int main()
             // Добавить символ к слову
             word[index] = c;
             index++;
-
             // Игнорировать слова длиннее чем 45 символов
             igonoreBigWords(fp, c, index);
         }
@@ -64,35 +91,8 @@ int main()
             index = 0;
         }
     }
-
-    fclose(fp);
-    dicts -> unload();
-
-    cout << "\nWORDS MISSPELLED: " << misspellings << endl;
-    cout << "WORDS IN DICTIONARY: " << dicts -> size() << endl;
-    cout << "WORDS IN TEXT: " << words << endl;
-
-    delete dicts;
-    return 0;
-}
-
-char *getFile()
-{
-    char *text = new char(LENGTH);
-    cout << "Enter the name of a file you'd like to check: ";
-    cin >> text;
-    return text;
-}
-
-bool openForReading(FILE *f)
-{
-    if (f == NULL)
-    {
-        cout << "Could not open a text. Usage: file.txt" << endl;
-        return true;
-    }
-
-    return false;
+    // Вывести результаты на экран
+    showResults(misspellings, words, dicts);
 }
 
 void igonoreBigWords(FILE *fp, char c, int index)
@@ -105,13 +105,12 @@ void igonoreBigWords(FILE *fp, char c, int index)
     }
 }
 
-bool checkWord(int index, char *word, Dictionary *d)
+bool checkWord(int index, char *word, Dictionary *dicts)
 {
     // Добавить '\0' в конец строки
     word[index] = '\0';
-
     // Если текущего слова нет в словаре - вывести на экран
-    if (!d -> check(word))
+    if (!dicts -> check(word))
     {
         cout << endl << word;
         return true;
@@ -120,4 +119,9 @@ bool checkWord(int index, char *word, Dictionary *d)
     return false;
 }
 
-
+void showResults(int misspellings, int words, Dictionary *dicts)
+{
+    cout << "\nWORDS MISSPELLED: " << misspellings << endl;
+    cout << "WORDS IN DICTIONARY: " << dicts -> size() << endl;
+    cout << "WORDS IN TEXT: " << words << endl;
+}
